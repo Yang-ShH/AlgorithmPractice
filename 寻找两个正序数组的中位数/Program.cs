8be675f2057bb78b76a837cf9c323a364d1af6d0
Yang-ShH <1029586711@qq.com>
2022-03-27 10:00:29 +0800

    update

---
+++ 寻找两个正序数组的中位数/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        var result = FindMedianSortedArrays(new int[] {1,1}, new int[] {1,2});
+    }
+
+    public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        var nums = nums1.Concat(nums2).OrderBy(e => e).ToArray();
+        var len = nums.Length;
+        if (len % 2 == 0)
+        {
+            return (double)(nums[len / 2] + nums[(len / 2) - 1]) / 2;
+        }
+        else
+        {
+            return (double)nums[(len - 1) / 2];
+        }
+    }
+}
