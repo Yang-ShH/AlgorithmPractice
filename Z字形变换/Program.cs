973d59264baf26382255d0794e21230b669ce276
Yang-ShH <1029586711@qq.com>
2022-03-30 20:49:36 +0800

    update

---
+++ Z字形变换/Program.cs
@@ -0,0 +1,51 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        var result = Convert("abcdefghijklmnopqrst", 5);
+    }
+    public static string Convert(string s, int numRows)
+    {
+        if (numRows < 2 || s.Length <= numRows)
+        {
+            return s;
+        }
+
+        var charLists = new List<List<char>>();
+        for (int i = 0; i < numRows; i++)
+        {
+            charLists.Add(new List<char>());
+        }
+
+        int times = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i <= numRows - 2)
+            {
+                charLists[i].Add(s[i]);
+            }
+            else
+            {
+                var remainder = i % (numRows - 1);
+                if (remainder == 0)
+                {
+                    charLists.Reverse();
+                    times++;
+                }
+                charLists[remainder].Add(s[i]);
+            }
+        }
+        if (times % 2 != 0)
+        {
+            charLists.Reverse();
+        }
+        string result = "";
+        foreach (var charList in charLists)
+        {
+            var tempStr = string.Join("", charList);
+            result = result + tempStr;
+        }
+        return result;
+    }
+}
