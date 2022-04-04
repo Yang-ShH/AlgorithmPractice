1ca85364b833ac8ef4e35d480e54cb69600b7655
杨士航 <yangsh@genepoint.cn>
2022-04-04 11:33:11 +0800

    update

---
+++ 回文数/Program.cs
@@ -0,0 +1,51 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        var x = 12321;
+        var result = IsPalindrome(x);
+        var result2 = IsPalindrome2(x);
+    }
+
+    public static bool IsPalindrome(int x)
+    {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+        {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while (x > revertedNumber)
+        {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+
+    public static bool IsPalindrome2(int x)
+    {
+        try
+        {
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+
+            var temp = x;
+            int revertedNumber = 0;
+            while (temp > 0)
+            {
+                revertedNumber = revertedNumber * 10 + temp % 10;
+                temp /= 10;
+            }
+
+            return x == revertedNumber;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+}
