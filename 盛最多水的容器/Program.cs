0b88d11c0ddea8658b5334f4b0956cb48eb01de0
Shihang Yang <1029586711@qq.com>
2022-04-10 10:31:48 +0800

    update

---
+++ 盛最多水的容器/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        _ = MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 });
+    }
+
+    public static int MaxArea(int[] height)
+    {
+        var r = height.Length - 1;
+        var l = 0;
+        var ans = 0;
+        while (l < r)
+        {
+            var s = Math.Min(height[l], height[r]) * (r - l);
+            ans = Math.Max(ans, s);
+            if (height[l] > height[r])
+            {
+                r--;
+            }
+            else
+            {
+                l++;
+            }
+        }
+        return ans;
+    }
+}
