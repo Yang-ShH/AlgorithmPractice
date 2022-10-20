3553bae095707d5cc5acade3a354be959eb133fb
杨士航 <yangsh@genepoint.cn>
2022-10-20 11:39:51 +0800

    delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,24 @@
+﻿public class DelegateTest
+{
+    public delegate void MyDelegate(int num);
+
+    public static int P = 10;
+    public static void Add(int num)
+    {
+        P += num;
+    }
+
+    public static void Multiplication(int num)
+    {
+        P *= num;
+    }
+
+    public static void Main()
+    {
+        MyDelegate d1 = new MyDelegate(Add);
+        d1 += new MyDelegate(Multiplication);
+        d1(10);
+        Console.WriteLine($"P = {P}");
+        Console.ReadLine();
+    }
+}
