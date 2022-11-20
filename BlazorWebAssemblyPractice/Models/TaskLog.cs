ec09c9a68f9f00860c3c7a5c848b2ee1695aebc3
Shihang Yang <1029586711@qq.com>
2022-11-20 10:56:39 +0800

    add Blazor WebAssembly practice

---
+++ BlazorWebAssemblyPractice/Models/TaskLog.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorWebAssemblyPractice.Models
+{
+    public class TaskLog
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// 操作人
+        /// </summary>
+        public string Operator { get; set; }
+        /// <summary>
+        /// 任务编号
+        /// </summary>
+        public string TaskId { get; set; }
+        /// <summary>
+        /// 任务类型
+        /// </summary>
+        public short Type { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+    }
+}
