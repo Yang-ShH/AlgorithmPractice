8ab3b777b75fdbdda81353d636add6ef49fc7094
杨士航 <yangsh@genepoint.cn>
2022-12-07 13:07:15 +0800

    添加对象去重

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,4 +1,6 @@
-﻿public class DelegateTest
+﻿using System.Diagnostics.CodeAnalysis;
+
+public class DelegateTest
 {
     public delegate void MyDelegate(int num);
 
@@ -20,5 +22,50 @@
         d1(10);
         Console.WriteLine($"P = {P}");
         Console.ReadLine();
+    }
+
+
+    public void ObjDistinct()
+    {
+        var list = new List<WaitingMovingRacksInfo>()
+        {
+            new WaitingMovingRacksInfo()
+            {
+                RackStorageId = 1,
+                RackCode = "123",
+                RackType = 258,
+                SumPosNum = 96
+            },
+            new WaitingMovingRacksInfo()
+            {
+                RackStorageId = 1,
+                RackCode = "123",
+                RackType = 258,
+                SumPosNum = 96
+            },
+        };
+        var list1 = list.Distinct().ToList();
+        var list2 = list.Distinct(new WaitingMovingRacksInfoComparer()).ToList();
+    }
+
+    public class WaitingMovingRacksInfo
+    {
+        public int RackStorageId { get; set; }
+        public short RackType { get; set; }
+        public string RackCode { get; set; }
+        public int SumPosNum { get; set; }
+    }
+
+    public class WaitingMovingRacksInfoComparer : IEqualityComparer<WaitingMovingRacksInfo>
+    {
+        public bool Equals(WaitingMovingRacksInfo? x, WaitingMovingRacksInfo? y)
+        {
+            return x?.RackCode.Equals(y?.RackCode) ?? false;
+        }
+
+        public int GetHashCode([DisallowNull] WaitingMovingRacksInfo obj)
+        {
+            return obj.RackCode.GetHashCode();
+        }
     }
 }
